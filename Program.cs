7b1a5c771ba0f3ace0f77d8bc53155be0efc97a5
EdgardoRomeroCruz <earomerocruz@unitec.edu>
2024-11-07 17:31:17 -0600

    buscando que no se salga

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@
  */
 
 using Examen;
+using System.Linq.Expressions;
 
 class Program
 {
@@ -32,9 +33,86 @@ class Program
             Console.WriteLine("docente");
             asignatura.NombreDocente = Console.ReadLine();
 
-            Console.WriteLine("Nota 1 maximo 30%");
+            
+            //valores de las notas hasta que sea valido sin botar
+            bool notaValida = false;
+            //------------------Nota 1--------------------------//
+            notaValida = false;
+            while (!notaValida)
+
+            {
+                try
+                {
+                    Console.WriteLine("Nota 1 (máximo 30%):");
+                    asignatura.N1 = int.Parse(Console.ReadLine());
+
+                    if (asignatura.N1 <= 30)
+                    {
+                        notaValida = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: La nota debe ser menor o igual que 30%.");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Error: Por favor ingresa un valor numérico.");
+                }
+            }
+            //------------------Nota 2--------------------------//
+            notaValida = false;
+            while (!notaValida)
+
+            {
+                try
+                {
+                    Console.WriteLine("Nota 2 (máximo 30%):");
+                    asignatura.N2 = int.Parse(Console.ReadLine());
+
+                    if (asignatura.N2 <= 40)
+                    {
+                        notaValida = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: La nota debe ser menor o igual que 30%.");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Error: Por favor ingresa un valor numérico.");
+                }
+
+            }
+            //------------------Nota 3--------------------------//
+            notaValida = false;
+            while (!notaValida)
+
+            {
+                try
+                {
+                    Console.WriteLine("Nota 3 (máximo 40%):");
+                    asignatura.N3 = int.Parse(Console.ReadLine());
+
+                    if (asignatura.N3 <= 40)
+                    {
+                        notaValida = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: La nota debe ser menor o igual que 40%.");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Error: Por favor ingresa un valor numérico.");
+                }
+            }
+            //--------------esto me bota al haber error------------------------//
+            /*Console.WriteLine("Nota 1 maximo 30%");
             asignatura.N1 = int.Parse(Console.ReadLine());
-            if (asignatura.N1 > 30)
+             if (asignatura.N1 > 30)
                 throw new Exception("la nota debe ser menor o igual que 30%");
             Console.WriteLine("Nota 2 maximo 30%");
             asignatura.N2 = int.Parse(Console.ReadLine());
@@ -43,7 +121,7 @@ class Program
             Console.WriteLine("Nota 2 maximo 40%");
             asignatura.N3 = int.Parse(Console.ReadLine());
             if (asignatura.N3 > 40)
-                throw new Exception("la nota debe ser menor o igual que 40%");
+                throw new Exception("la nota debe ser menor o igual que 40%");*/
             asignatura.Imprimir();
         }
         catch (Exception ex)
